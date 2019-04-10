eb3e137606f3eb0caa2cae51ca448e4032f9ee1f
Brian Kwon <bkwon94@gmail.com>
2019-04-09 22:19:19 -0400

    All modules rendering but still many server issues

--- server/index.js
+++ server/index.js
@@ -12,3 +12,27 @@ app.use(express.static('public'));
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
+
+
+
+// video carousel
+const videoCarouselOptions = {
+  target: 'http:localhost:3333',
+  changeOrigin: true
+};
+const videoCarouselProxy = proxy(videoCarouselOptions);
+app.use('/associatedVideos', videoCarouselProxy);
+
+
+const castCrewOptions = {
+  target: 'http:localhost:2002'
+};
+const castCrewProxy = proxy(castCrewOptions);
+app.use('/actors', castCrewProxy);
+
+
+const movieInfoOptions = {
+  target: 'http:localhost:2000'
+};
+const movieInfoProxy = proxy(movieInfoOptions);
+app.use('/movies', movieInfoProxy);
