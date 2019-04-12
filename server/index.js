8845ff37a3b1ceb509383d7dca35499343f66c33
Brian Kwon <bkwon94@gmail.com>
2019-04-11 21:51:13 -0400

    Replace my own bundle with one hosted on S3

--- server/index.js
+++ server/index.js
@@ -9,6 +9,15 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 // serve up static file
 app.use(express.static('public'));
+app.use('/*/styles.css', express.static('public/styles.css'));
+app.use('/*', express.static('public'));
+
+//
+// app.use(express.static(__dirname + '/../client/dist'));
+// app.use('/*/styles.css', express.static(__dirname + '/../client/dist/styles.css'));
+// app.use('/*', express.static(__dirname + '/../client/dist'));
+
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
@@ -18,7 +27,7 @@ app.listen(PORT, () => {
 // video carousel
 const videoCarouselOptions = {
   target: 'http://localhost:3333',
-  changeOrigin: true
+  // changeOrigin: true
 };
 const videoCarouselProxy = proxy(videoCarouselOptions);
 app.use('/associatedVideos', videoCarouselProxy);
@@ -26,14 +35,15 @@ app.use('/associatedVideos', videoCarouselProxy);
 
 const castCrewOptions = {
   target: 'http://localhost:2002',
-  changeOrigin: true
+  // changeOrigin: true
 };
 const castCrewProxy = proxy(castCrewOptions);
 app.use('/actors', castCrewProxy);
 
 
 const movieInfoOptions = {
-  target: 'http://localhost:2000'
+  target: 'http://localhost:2000',
+  // changeOrigin: true
 };
 const movieInfoProxy = proxy(movieInfoOptions);
 app.use('/movies', movieInfoProxy);
